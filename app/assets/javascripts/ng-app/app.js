252e562fb83f307779429d88193da4805c180c1c
marcwright <marc.wright@gmail.com>
2014-12-16 10:25:11 -0500

    first commit for resume ui-router

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,68 @@
+
+angular
+	.module('myApp', [
+		'ngAnimate',
+		'ui.router',
+		'templates'
+	])
+	.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+		/**
+		 * Route and States
+		 */
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'home.html',
+				controller: 'HomeCtrl'
+			})
+			// an abstract state that just serves as a
+			// parent for the below child states
+			.state('dashboard', {
+				abstract: true,
+				url: '/dashboard',
+				templateUrl: 'dashboard/layout.html'
+			})
+				// the default route when someone hits dashboard
+				.state('dashboard.one', {
+					url: '',
+					templateUrl: 'dashboard/one.html'
+				})
+				// this is /dashboard/two
+				.state('dashboard.two', {
+					url: '/two',
+					templateUrl: 'dashboard/two.html'
+				})
+				// this is /dashboard/three
+				.state('dashboard.three', {
+					url: '/three',
+					templateUrl: 'dashboard/three.html'
+				})
+				.state('resume', {
+				abstract: true,
+				url: '/resume',
+				templateUrl: 'resume/layout.html'
+			})
+				// the default route when someone hits dashboard
+				.state('resume.four', {
+					url: '',
+					templateUrl: 'resume/four.html'
+				})
+				// this is /dashboard/two
+				.state('resume.five', {
+					url: '/five',
+					templateUrl: 'resume/five.html'
+				})
+				// this is /dashboard/three
+				.state('resume.six', {
+					url: '/six',
+					templateUrl: 'resume/six.html'
+				});;
+
+		// default fallback route
+		$urlRouterProvider.otherwise('/');
+
+		// enable HTML5 mode for SEO
+		$locationProvider.html5Mode(true);
+
+	});
