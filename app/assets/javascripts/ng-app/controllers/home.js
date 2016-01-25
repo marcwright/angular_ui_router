bd8d2d21ca3ce4c30036a9af498fe07609ca1841
marcwright <marc.wright@gmail.com>
2016-01-25 16:47:50 -0500

    about to add new projects

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,12 @@
 angular.module('myApp')
 	.controller('HomeCtrl', ['$scope', function ($scope) {
 			$scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
+
+      $(function(){
+        $(".element").typed({
+            strings: ["First sentence.", "Second sentence."],
+            typeSpeed: 0
+        });
+    });
+      
 	}]);
